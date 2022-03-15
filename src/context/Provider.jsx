e81136f44c371c16c403fa667dc29981bfc6ae83
mroenca40 <mproenca.marketing@gmail.com>
2022-03-15 19:30:22 -0300

    adiciona axios de login

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,11 +1,20 @@
 import Context from './Context';
+import PropTypes from 'prop-types';
 
 function Provider({ children }) {
+
   return (
     <Context.Provider>
       { children}
     </Context.Provider>
   )
 }
+
+Provider.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
 
 export default Provider;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,19 +1,40 @@
 import './styles.css';
-
+import React, { useState } from 'react';
+import axios from 'axios';
 function Login() {
+  const [email, setEmail] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  async function attemptLogin() {
+    try {
+      const { data } = await axios.post('https://my-json-server.typicode.com/tractian/fake-api/users', {
+        email: email,
+      })
+      localStorage.setItem('user', JSON.stringify(data))
+      setLoading(false)
+    } catch (error) {
+      console(error)
+    }
+  }
+
   return (
     <div className="container">
       <div className="content-login row">
         <div className="content-left col-6">
           <img className="img-fluid" src="https://app.tractian.com/static/media/tractian-login.f480cb7e.svg" alt="traction-login" />
         </div>
-        <div className="content-right col-6 align-items-stretch">
+        <form className="content-right col-6 align-items-stretch">
           <h1>LOGIN</h1>
           <p>Insira seu e-mail</p>
           <div class="input-group has-validation content-input">
             <span class="input-group-text" id="inputGroupPrepend">@</span>
-            <input type="text"
-            class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
+            <input 
+              type="email"
+              placeholder='E-mail'
+              onChange={({ target }) => setEmail(target.value)}
+              class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend"
+              required
+            />
               <div class="invalid-feedback">
                 Please choose a username.
               </div>
@@ -21,10 +42,12 @@ function Login() {
           <div>
             <button
               className="content-btn btn btn-primary col-12"
-              type='submit'
+              type='button'
+              onClick={ attemptLogin }
             >Acessar</button>
           </div>
-        </div>
+          {loading ? <p>Loading</p> : null}
+        </form>
       </div>
     </div>
   )
